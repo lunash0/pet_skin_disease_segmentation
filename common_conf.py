d54029ae0389f7d0d27499a2ccdd2dd60619ce9b
lunash <101923529+lunash0@users.noreply.github.com>
2023-08-23 00:16:28 +0900

    Create common_conf.py

---
+++ common_conf.py
@@ -0,0 +1,18 @@
+#################################################
+# for function.py
+#################################################
+
+# model input image size
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = IMAGE_WIDTH
+
+# for resize image
+IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+
+# for model input image shape
+IMAGE_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT, 3)
+
+# for model output image shape
+MASK_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT, 2)
+
+EXTENSION_TFRECORDS = '.tfrecords'
