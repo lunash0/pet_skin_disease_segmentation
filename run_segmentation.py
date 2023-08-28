77ff9c40f72300d7b7ca467530f5643e52866c1b
lunash <101923529+lunash0@users.noreply.github.com>
2023-08-28 16:09:33 +0900

    Update run_segmentation.py

--- run_segmentation.py
+++ run_segmentation.py
@@ -1,4 +1,8 @@
+# run_segmentation.py
+
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import json
 import requests
 from io import BytesIO
 
@@ -6,12 +10,11 @@ import time
 
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-from PIL import Image
 
 import tensorflow as tf
 
-import function
+from . import function
+from django.conf import settings
 
 
 # get best weight & class_id
@@ -28,6 +31,7 @@ def check_model_path(model_path):
 # load model
 def load_model(latest, lr):
     model = function.build_model(lr)
+    # -----------------------------------------
     model.load_weights(latest)
     return model
 
@@ -48,7 +52,7 @@ def overlay(image, mask, color=(0, 0, 200), alpha=0.5, resize=None):
         image_combined: The combined image. np.ndarray
 
     """
-    color = color[::-1]
+    color = color[::-1] # for using openCV
     colored_mask = np.expand_dims(mask, 0).repeat(3, axis=0)
     colored_mask = np.moveaxis(colored_mask, 0, -1)
     masked = np.ma.MaskedArray(image, mask=colored_mask, fill_value=color)
@@ -58,7 +62,7 @@ def overlay(image, mask, color=(0, 0, 200), alpha=0.5, resize=None):
         image = cv2.resize(image.transpose(1, 2, 0), resize)
         image_overlay = cv2.resize(image_overlay.transpose(1, 2, 0), resize)
 
-    image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0)
+    image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0) # ��???���� 0??? 'Alpha Blending'??? ?????? ����??? ???�߿� ??? ????????? ??????����
 
     return image_combined
 
@@ -66,54 +70,20 @@ def overlay(image, mask, color=(0, 0, 200), alpha=0.5, resize=None):
 CLS_THRESHOLD = 0.03
 CONF_THRESHOLD = 0.7
 IMAGE_SIZE = (224, 224)
-# MODEL_PATH = "./workspace/data/dog/result_segm/U-Net_A1_200_8_0.00049974_(224, 224, 3)_(224, 224, 2)"  # A1 model path
 
 MODEL_NAMES = ['A1', 'A3']
-MODEL_PATHS = [
-    # "./workspace/data/dog/result_segm/U-Net_A1_200_8_0.00049974_(224, 224, 3)_(224, 224, 2)",  # A1 model path
-    "./workspace/data/dog/result_segm/U-Net_A3_200_8_0.00012834_(224, 224, 3)_(224, 224, 2)"  # A3 model path
-]
+model_A1_path = os.path.join(settings.MODEL_ROOT, 'U-Net_A1_200_8_0.00049974_(224, 224, 3)_(224, 224, 2)')
+model_A3_path = os.path.join(settings.MODEL_ROOT, 'U-Net_A3_200_8_0.00012834_(224, 224, 3)_(224, 224, 2)')
+MODEL_PATHS = [model_A1_path, model_A3_path]
 
-
-if __name__ == "__main__":
-
+def run_segm_model(img, img_vis):
     models = []
     for model_path in MODEL_PATHS:
         # Load model info
-        latest, class_id, lr = check_model_path(model_path)
-
+        latest, _, lr = check_model_path(model_path) # MODEL+PATHS?? ??????���??? ???????????? ??? ??? ???????? ?????????
         # Build mode & load trained weight
         model = load_model(latest, lr)
         models.append(model)
-
-    # while True:
-    # TODO: AWS 서버에 이미지 업로드 되었는지 체크하는 코드 작성
-
-    # TODO: if new_data_arrived:
-    #           run code
-    #       else:
-    #           wait
-
-    # Load image form local (Temporary)
-    img = Image.open('./test/aihub_tmp1.jpg')
-
-    # Get image from server
-    # url = 'https://mblogthumb-phinf.pstatic.net/MjAxODEwMDNfMjE2/MDAxNTM4NTUwNzUwOTEx.mMEklChU0H7zC-A41Ti478q53ZT4Qnr0ZZp8hUdhaiog.TuL41isBADprznlsD58MP2oL3ZG9GmHVa8w8rpfSEp4g.PNG.hansolpet/%EA%B0%95%EC%95%84%EC%A7%80_%ED%94%BC%EB%B6%80%EC%97%BC%EC%A6%9D.PNG?type=w800'
-    # res = requests.get(url)
-    # img = Image.open(BytesIO(res.content))
-
-    # Preprocess (resize, normalization)
-
-    #TEST
-    # canvas = np.ones((224, 224, 3), dtype=np.uint8)*210
-    # img = img.resize((224, 224))
-    # img = np.array(img)[:180, :180]
-    # canvas[:180, :180, :] = np.array(img)
-    # img = canvas
-
-    img = img.resize(IMAGE_SIZE)
-    img_vis = np.array(img)  # copy image for visualization
-    img = np.array(img) / 255.
 
     pred_clses = []
     pred_masks = []
@@ -127,22 +97,15 @@ if __name__ == "__main__":
         pred_mask[~pred_valid] = 0.  # ignore invalid predictions
 
         # Do classification
-        if pred_valid.sum() / (224 * 244) > CLS_THRESHOLD:
+        if pred_valid.sum() / (224 * 244) > CLS_THRESHOLD - 0.025:
             pred_clses.append(True)
         else:
             pred_clses.append(False)
         pred_masks.append(pred_mask)
 
+    img_overlays = [] # List to store generated img_overlay images
     for model_name, pred_mask in zip(MODEL_NAMES, pred_masks):
         img_overlay = overlay(img_vis, pred_mask, color=(200, 0, 0), alpha=0.3)
-
-        plt.imshow(img_overlay)
-        # plt.title(model_name)
-        plt.show()
-
-    # TODO: send predicted result to server
-
-    # break
-
-    # t=1
-
+        img_overlays.append(img_overlay)
+    
+    return img_overlays, pred_clses
